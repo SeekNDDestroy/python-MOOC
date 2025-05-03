d0af53fc28606035427f585598ac662c6a4d39fd
rahul <rahulkandolkar@gmail.com>
2025-05-03 15:44:04 +0530

    part 1 complete

---
+++ part 1/Name and address.py
@@ -0,0 +1,9 @@
+# Write your solution here
+name = input("Given name:")
+familyName = input("Family name:");
+streetAddress = input("Street address:")
+cityAndPostal = input("City and postal code:")
+
+print(name,familyName)
+print(streetAddress)
+print(cityAndPostal)
